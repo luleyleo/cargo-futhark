815a6ecc772e6002ff2f1b80fd069471ef8bad2d
Leopold Luley <git@leopoldluley.de>
2023-07-22 21:15:11 +0200

    Split example into library and binary.

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,7 +1,11 @@
-mod futhark {
-    include!(concat!(env!("OUT_DIR"), "/futhark/futhark_lib.rs"));
-}
+type Backend = simple_lib::backends::C;
 
 fn main() {
-    println!("Hello, world!");
+    let config = simple_lib::Config::<Backend>::new();
+    let context = simple_lib::Context::new(config);
+
+    let input = &[1.0, 2.0, 3.0];
+    let average = 2.0;
+
+    assert_eq!(average, 2.0);
 }
