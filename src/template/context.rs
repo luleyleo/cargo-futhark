5f3f5883a339ce2fd61b86d8ab35c7f0ff0679f2
Leopold Luley <git@leopoldluley.de>
2023-07-22 22:58:57 +0200

    Generate entry functions on Context.

--- src/template/context.rs
+++ src/template/context.rs
@@ -1,7 +1,7 @@
 use proc_macro2::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 
-use crate::manifest::{EntryPoint, Manifest};
+use crate::manifest::{EntryPoint, Manifest, Type};
 
 pub fn context(manifest: &Manifest) -> TokenStream {
     let entry_fns = manifest.entry_points.iter().map(context_entry_fn);
@@ -49,6 +49,101 @@ pub fn context(manifest: &Manifest) -> TokenStream {
     }
 }
 
-fn context_entry_fn(_ep: &EntryPoint) -> TokenStream {
-    quote!()
+fn context_entry_fn(ep: &EntryPoint) -> TokenStream {
+    let name = format_ident!("{}", ep.fn_name());
+    let context_name = format_ident!("{}", ep.context_fn_name());
+
+    let in_params = ep.inputs.iter().enumerate().map(|(i, input)| {
+        let name = format_ident!("in_{}", i);
+
+        match input {
+            Type::Value(value) => {
+                let typ = format_ident!("{}", value.rust_name());
+
+                quote!(#name: #typ)
+            }
+            Type::Array(array) => {
+                let typ = format_ident!("{}", array.struct_name());
+
+                quote!(#name: &#typ<B>)
+            }
+        }
+    });
+
+    let out_params = ep.outputs.iter().map(|input| match input {
+        Type::Value(value) => {
+            let typ = format_ident!("{}", value.rust_name());
+
+            quote!(#typ)
+        }
+        Type::Array(array) => {
+            let typ = format_ident!("{}", array.struct_name());
+
+            quote!(#typ<B>)
+        }
+    });
+
+    let let_out_vars = ep.outputs.iter().enumerate().map(|(i, input)| {
+        let name = format_ident!("out_{}", i);
+
+        match input {
+            Type::Value(value) => {
+                let typ = format_ident!("{}", value.rust_name());
+
+                quote!(let mut #name: #typ = Default::default();)
+            }
+            Type::Array(array) => {
+                let typ = format_ident!("{}", array.struct_name());
+
+                quote! {
+                    let mut #name = #typ {
+                        context: self,
+                        inner: std::ptr::null_mut(),
+                    };
+                }
+            }
+        }
+    });
+
+    let out_vars_params = ep
+        .outputs
+        .iter()
+        .enumerate()
+        .map(|(i, typ)| (format_ident!("out_{}", i), typ))
+        .map(|(ident, typ)| match typ {
+            Type::Value(_) => quote!(#ident),
+            Type::Array(_) => quote!(#ident.inner),
+        });
+
+    let in_vars_params = ep
+        .inputs
+        .iter()
+        .enumerate()
+        .map(|(i, typ)| (format_ident!("in_{}", i), typ))
+        .map(|(ident, typ)| match typ {
+            Type::Value(_) => quote!(#ident),
+            Type::Array(_) => quote!(#ident.inner),
+        });
+
+    let out_vars = ep
+        .outputs
+        .iter()
+        .enumerate()
+        .map(|(i, _)| format_ident!("out_{}", i));
+
+    quote! {
+        #[allow(unused_parens)]
+        pub fn #context_name(&self, #(#in_params),*) -> Result<(#(#out_params),*), i64> {
+            #(#let_out_vars)*
+
+            let status = unsafe {
+                B::#name(self.inner, #(&mut #out_vars_params),*, #(#in_vars_params),*)
+            };
+
+            match status {
+                0 => Ok((#(#out_vars),*)),
+                err => Err(err.into()),
+            }
+        }
+    }
 }
