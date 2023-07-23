b887bdef2960f38bda029f84e2ce66d5e6734a55
Leopold Luley <git@leopoldluley.de>
2023-07-23 22:17:05 +0200

    Add documentation to generator libarary.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+#![warn(missing_docs)]
+
+//! # cargo-futhark
+//!
+//! A library and cargo subcommand to conveniently integrate [Futhark](https://futhark-lang.org/) into Rust projects.
+//!
+//! See the [usage section](https://github.com/luleyleo/cargo-futhark#usage) in the readme to get started with the CLI.
+//! This documentation will focus on the code generation aspect of this crate.
+//!
+//! ## Generating Bindings
+//!
+//! The core of this library is the [`Generator`].
+//! Using it, you can automatically generate safe bindings for your Futhark code.
+//! Take a look at its documentation to understand how it works.
+//!
+//! ## Using Generated Bindings
+//!
+//! Once you've generated the bindings, including them is as easy as this:
+//! ```ignore
+//! include!(concat!(env!("OUT_DIR"), "/futhark/futhark_lib.rs"));
+//! ```
+//!
+
 mod manifest;
 mod template;
 
